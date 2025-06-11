58f1c29f4cd19a5504f3c2d737c59d9c3b086e0c
dlrhksdn <lkukgr@naver.com>
2025-06-11 13:45:24 +0900

    [ + ]

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -4,11 +4,11 @@
 int dive(int x, int y)
 {
 	if (y == 0) return 0;
-	int count = 0;
+	int cou = 0;
 	int temp = x;
 	while (temp >= y) {
 		temp = sub(temp, y);
-		count++;
+		cou++;
 	}
-	return count;
+	return cou;
 }
